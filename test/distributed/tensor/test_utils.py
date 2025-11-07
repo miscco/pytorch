52adbb36782474b07109822c426820231f7dddc0
Will Constable <whc@meta.com>
2025-11-07 14:36:13 -0800

    [DTensor] Make ExplicitRedistributeContext strict/non-strict mode
    Also support nesting, enable/disable, and make the class use a
    thread-local for storage so independent threads do not confuse each
    other.
    
    ghstack-source-id: aee48a0c29e659c5b158f6294b46f857af2a6d0a
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/167370

--- test/distributed/tensor/test_utils.py
+++ test/distributed/tensor/test_utils.py
@@ -6,6 +6,7 @@ from typing import Any
 
 import torch
 import torch.distributed as dist
+from torch.autograd import Function
 from torch.distributed._local_tensor import (
     local_tensor_mode,
     LocalTensor,
@@ -938,13 +939,26 @@ class TestExplicitRedistribute(LocalTensorTestBase):
 
             dx = distribute_tensor(x, device_mesh, [Shard(0)])
             dA = distribute_tensor(A, device_mesh, [Replicate()])
-            with ExplicitRedistributionContext():
+            with ExplicitRedistributionContext(strict=True):
                 dY = torch.matmul(dx, dA_repl)
                 loss = dY.sum()
 
                 # we now see the error during backwards
                 with self.assertRaisesRegex(RuntimeError, "Implicit redistribution"):
-                    loss.backward()
+                    loss.backward(retain_graph=True)
+
+                with ExplicitRedistributionContext(strict=False):
+                    # but since it's a 'free' redistribute, we can still do it under non-strict mode
+                    loss.backward(retain_graph=True)
+
+                with ExplicitRedistributionContext(enable=False):
+                    # and we can disable
+                    loss.backward(retain_graph=True)
+
+                # and re-enable
+                with self.assertRaisesRegex(RuntimeError, "Implicit redistribution"):
+                    loss.backward(retain_graph=True)
+
 
 
 if __name__ == "__main__":
